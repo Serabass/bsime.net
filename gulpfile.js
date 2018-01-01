a04aa64047ae700ee00e146812caf8c6794f1cea
serab <is_everything@mail.ru>
2018-01-01 14:33:13 +0600

    123

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,78 @@
 var gulp = require('gulp'),
   jade = require('gulp-jade'),
   sass = require('gulp-sass'),
-  log = require('gulp-log');
+  log = require('gulp-log'),
+
+  // TODO use https://github.com/morris/vinyl-ftp
+  ftp = require('vinyl-ftp'),
+  gutil = require('gulp-util'),
+  path = require('path');
+
+
+var paths = {
+  templates: ['index.jade'],
+  styles: ['./assets/css/*.sass']
+};
+
+require('dotenv').config();
+
+function ftpDefaults() {
+  var args = [].slice.call(arguments);
+  args.unshift(process.env.FTP_REMOTE_PATH);
+  var remotePath = path.join.apply(path, args);
+  return ftp({
+    host: process.env.FTP_HOST,
+    user: process.env.FTP_USER,
+    pass: process.env.FTP_PASSWORD,
+    remotePath: remotePath
+  });
+}
 
 gulp.task('templates', function() {
-  return gulp.src('index.jade')
+  return gulp.src(paths.templates)
     .pipe(jade())
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('sass', function() {
-  return gulp.src('./assets/css/*.sass')
-    .pipe(log())
+  return gulp.src(paths.styles)
     .pipe(sass())
     .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('default', ['templates', 'sass']);
+gulp.task('deploy', ['build'], function() {
+
+  var conn = ftp.create({
+    host: process.env.FTP_HOST,
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    parallel: 10,
+    log: gutil.log
+  });
+
+  var globs = [
+    'assets/css/main.css',
+    'assets/css/fonts/**',
+    'assets/img/**/**',
+    'assets/js/**/**',
+    'letters/**/**',
+    'index.html'
+  ];
+
+  // using base = '.' will transfer everything to /public_html correctly
+  // turn off buffering in gulp.src for best performance
+
+  return gulp.src(globs, {base: '.', buffer: false})
+  //.pipe(log())
+    .pipe(conn.newer('/')) // only upload newer files
+    .pipe(conn.dest('/'));
+
+});
+
+gulp.task('serve', function () {
+  gulp.watch(paths.templates, ['templates']);
+  gulp.watch(paths.styles, ['sass']);
+});
+gulp.task('build', ['templates', 'sass']);
+
+gulp.task('default', ['build']);
