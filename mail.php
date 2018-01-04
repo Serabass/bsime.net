c9bc043ff1b03948dbb372d5fb9bf8eb5a0fc40c
serab <is_everything@mail.ru>
2018-01-04 23:31:47 +0600

    123

--- mail.php
+++ mail.php
@@ -1,10 +1,40 @@
 <?php
-
+$enable_captcha = false;
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header('Location: /');
+    header('Location: /?err=1');
     exit;
 }
+if ($enable_captcha) {
+    if (empty($_POST['g-recaptcha-response'])) {
+        header('Location: /?err=2');
+        exit;
+    }
 
+    $captcha_response = $_POST['g-recaptcha-response'];
+    if (empty($captcha_response)) {
+        header('Location: /?err=3');
+        exit;
+    }
+    $captcha_response = urlencode($captcha_response);
+    $remoteip = $_SERVER['REMOTE_ADDR'];
+
+    $secret = file_get_contents('recaptcha_key');
+
+    if ($curl = curl_init()) {
+        curl_setopt($curl, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, "secret=$secret&response=$captcha_response&remoteip=$remoteip");
+        $out = curl_exec($curl);
+        $response = json_decode($out);
+        curl_close($curl);
+
+        if (empty($response) || empty($response['success']) || $response['success'] !== TRUE) {
+            header('Location: /?err=4');
+            exit;
+        }
+    }
+}
 $env = include('env.php');
 $con = new mysqli($env['MYSQL_HOST'],
     $env['MYSQL_USER'],
@@ -26,8 +56,6 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 
 $ip = $con->escape_string($ip);
 $values = "'$email', '$subject', '$message', '$ip'";
-$q = $con->query('INSERT INTO mail (`email`, `subject`, `message`, `ip`) VALUES ('.$values.')');
+$q = $con->query('INSERT INTO mail (`email`, `subject`, `message`, `ip`) VALUES (' . $values . ')');
 $con->close();
-
-header('Location: /');
-exit;
+header('Location: /?err=5');
